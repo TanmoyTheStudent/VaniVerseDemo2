49c6e10072891f24b3e61b8f7367ab769404e092
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-06 13:23:56 +0530

    Update HeaderResposive.js

--- src/components/header-footer/HeaderResposive.js
+++ src/components/header-footer/HeaderResposive.js
@@ -3,30 +3,43 @@ import Logo from "../../required-data/assests/image/Logo9.png"
 import './header.css'
 
 function HeaderResposive() {
+  const handleCloseOffcanvas = () => {
+    // Select the off-canvas element
+    const offcanvasElement = document.getElementById('offcanvasNavbar');
+    const offcanvas = window.bootstrap.Offcanvas.getInstance(offcanvasElement);
+    if (offcanvas) {
+      // Hide the off-canvas menu
+      offcanvas.hide();
+    }
+  };
+
   return (
     <div>
            <nav className="navbar navbar-light  navbar-expand-xl" style={{ backgroundColor: '#e7dedd' }}>
     <div className="container-fluid">
-       <div className="col-lg-6 col-md-10 col-10">
-          <Link
-              className="navbar-brand mb-0 h3 text-primary fs-1 fw-bold home-link"
-               to="/"
-              style={{ display: 'flex', alignItems: 'center' }} // Flex to align items vertically
-            >
-                <img
-                  src={Logo}
-                  alt="Vani Verse Logo"
-                  style={{
-                    maxWidth: '65%',
-                    height: 'auto',
-                    display: 'block',
-                    marginLeft: '5%', // Add gap to the left
-                  }}
-                />
-        </Link>
-      </div>
+   
+  <div className="col-xl-6 col-9">
+    <Link
+      className="navbar-brand mb-0 h3 text-primary fs-1 fw-bold home-link"
+      to="/"
+      style={{ display: 'flex', alignItems: 'center' }} // Flex to align items vertically
+    >
+      <img
+        src={Logo}
+        alt="Vani Verse Logo"
+        style={{
+          maxWidth: '65%',
+          height: 'auto',
+          display: 'block',
+          marginLeft: '5%', // Add gap to the left
+        }}
+      />
+    </Link>
+  </div>
 
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+
+
+    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" >
          <span className="navbar-toggler-icon"></span>
     </button>
 
@@ -34,18 +47,24 @@ function HeaderResposive() {
      style={{ backgroundColor: 'rgba(231, 222, 221, 0.8)' }} >
       <div className="offcanvas-header">
         <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+
+        <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/" onClick={handleCloseOffcanvas}>
         <img 
                         src={Logo}
                         alt="Vani Verse Logo" 
                         style={{ maxWidth: '65%', height: 'auto', display: 'block' }} 
                     />
+      </Link>
+        
+
+         
         </h5>
         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div className="offcanvas-body " >
         <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
           <li className="nav-item p-3 p-lg-0">
-          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/our-services">
+          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/our-services" onClick={handleCloseOffcanvas}>
           Services
       </Link>
           
@@ -53,26 +72,26 @@ function HeaderResposive() {
 
 
           <li className="nav-item p-3 p-lg-0">
-                 <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/participate&earn">
+                 <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/participate&earn" onClick={handleCloseOffcanvas}>
           Participate & Earn
       </Link>
           </li>
 
           <li className="nav-item p-3 p-lg-0">
-          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/about-us">
+          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/about-us" onClick={handleCloseOffcanvas}>
           About Us
       </Link>
           </li>
 
 
           <li className="nav-item p-3 p-lg-0">
-          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/login">
+          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/login" onClick={handleCloseOffcanvas} >
           Login 
       </Link>
           </li>
 
           <li className="nav-item p-3 p-lg-0">
-          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/register-yourself">
+          <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/register-yourself" onClick={handleCloseOffcanvas}>
           Register
       </Link>
           </li>
