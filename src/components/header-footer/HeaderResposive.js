db235616a6f78cf4a5d4571f330cbd639502bc75
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-09 15:48:57 +0530

    Update HeaderResposive.js

--- src/components/header-footer/HeaderResposive.js
+++ src/components/header-footer/HeaderResposive.js
@@ -1,39 +1,38 @@
+import { Box } from "@mui/material";
 import { Link } from "react-router-dom"
 import Logo from "../../required-data/assests/image/Logo9.png"
 import './header.css'
-import { Box } from "@mui/material";
+
 
 function HeaderResposive() {
   const handleCloseOffcanvas = () => {
-    // Select the off-canvas element
     const offcanvasElement = document.getElementById('offcanvasNavbar');
     const offcanvas = window.bootstrap.Offcanvas.getInstance(offcanvasElement);
     if (offcanvas) {
-      // Hide the off-canvas menu
       offcanvas.hide();
     }
   };
 
   return (
     <div>
-           <nav className="navbar navbar-light  navbar-expand-xl" style={{ backgroundColor: '#e7dedd' }}>
+           <nav className="navbar  navbar-expand-xl fixed-top" style={{ backgroundColor: 'white',boxShadow:'0 4px 8px rgba(0, 0, 0, 0.25)'  }}>
     <div className="container-fluid">
    
   <div className="col-xl-6 col-9">
     <Link
       className="navbar-brand mb-0 h3 text-primary fs-1 fw-bold home-link"
       to="/"
-      style={{ display: 'flex', alignItems: 'center' }} // Flex to align items vertically
+      style={{ display: 'flex', alignItems: 'center' }} 
     >
-       <Box
+      <Box
                 component="img"
                 src={Logo}
                 alt="Vani Verse Logo"
                 sx={{
-                  maxWidth: {lg:"65%",md:'60%',xs:"80%"},
+                  maxWidth: {lg:"65%",md:'55%',sm:"75%",xs:"80%"},
                   height: 'auto',
                   display: 'block',
-                  marginLeft: '5%', // Add gap to the left
+                  marginLeft: '5%',
                 }}
           />
     </Link>
@@ -65,7 +64,7 @@ function HeaderResposive() {
       </div>
       <div className="offcanvas-body " >
         <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
-          <li className="nav-item p-3 p-lg-0">
+          <li className="nav-item p-3 p-xl-0">
           <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/our-services" onClick={handleCloseOffcanvas}>
           Services
       </Link>
@@ -73,26 +72,26 @@ function HeaderResposive() {
           </li>
 
 
-          <li className="nav-item p-3 p-lg-0">
+          <li className="nav-item p-3 p-xl-0">
                  <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/participate&earn" onClick={handleCloseOffcanvas}>
           Participate & Earn
       </Link>
           </li>
 
-          <li className="nav-item p-3 p-lg-0">
+          <li className="nav-item p-3 p-xl-0">
           <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/about-us" onClick={handleCloseOffcanvas}>
           About Us
       </Link>
           </li>
 
 
-          <li className="nav-item p-3 p-lg-0">
+          <li className="nav-item p-3 p-xl-0">
           <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/login" onClick={handleCloseOffcanvas} >
           Login 
       </Link>
           </li>
 
-          <li className="nav-item p-3 p-lg-0">
+          <li className="nav-item p-3 p-xl-0">
           <Link className="navbar-brand mb-0 h3 mx-xl-3" to="/register-yourself" onClick={handleCloseOffcanvas}>
           Register
       </Link>
