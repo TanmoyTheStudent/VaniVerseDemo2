b8baa5e01f70e12b7182c119108dae5c94dbe284
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-11 10:57:33 +0530

    Update LanguagesWeWork.js

--- src/components/home/sub-components/LanguagesWeWork.js
+++ src/components/home/sub-components/LanguagesWeWork.js
@@ -1,47 +1,24 @@
-import { Paper,Typography } from "@mui/material"
+import { Paper,Typography,Container } from "@mui/material"
 import Languages from "./Lanuages"
 import LanguagesNext from "./LanguagesNext"
+import { design } from "../../../required-data/object-data/servicesInHome"
 
-const design={
-    padding: 2,
-    textAlign: 'center',
-    position: 'relative',
-    margin: 2,
-    //marginLeft:5,
-    //marginRigt:5,
-    backgroundColor: '#f3f1ee',
-    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
-    '&::after': {
-      content: '""',
-      display: 'block',
-      width: '100%',
-      height: '3px',
-      backgroundColor: '#008080',
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-    },
-  }
 
 function LanguagesWeWork() {
   return (
     <div>
-            <div 
-              style={{
-                marginBottom:"50px",marginTop:"8%"
-              }}
-            >
+     
         <Paper 
-      elevation={5} 
-      sx={design}
-    >
-      <Typography variant="h4" style={{fontFamily:"Georgia",wordSpacing: '0.2em'}}>
-        LANGUAGES WE WORK
-      </Typography>
-    </Paper>
-    </div>
-    <Languages/>
-    <LanguagesNext/>
+          elevation={5} 
+          sx={{...design,marginTop:{xs:"100px",md:"8%"},marginBottom:"50px"}}
+        >
+          <Typography variant="h4" style={{fontFamily:"Georgia",wordSpacing: '0.2em'}}>
+                LANGUAGES WE WORK
+          </Typography>
+        </Paper>
+    
+        <Languages/>
+        <LanguagesNext/>
 
     </div>
   )
