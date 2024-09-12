8414ac4c7903cd0aaf268d39005d600f48f0bc44
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-12 13:30:58 +0530

    Update associatedServiceDescription.js

--- src/required-data/object-data/associatedServiceDescription.js
+++ src/required-data/object-data/associatedServiceDescription.js
@@ -5,9 +5,9 @@ export const serviceDescription=[
         title:"Audio Segmentation Process",
         description:(
         <>
-        <b>1.	Audio Segmentation:</b> The provided audio file is divided into smaller, manageable segments<br/>
-        <b>2.	Non-Verbal Sound Identification:</b> We meticulously identify and mark pauses, noises, coughs, laughter, breathing sounds, humming, and other non-verbal sounds within the audio.<br/>
-        <b>3.	Speaker Identification:</b> For audio with multiple speakers, we identify and label each speaker within the segments.
+        <b>1. Audio Segmentation:</b> The provided audio file is divided into smaller, manageable segments<br/>
+        <b>2. Non-Verbal Sound Identification:</b> We meticulously identify and mark pauses, noises, coughs, laughter, breathing sounds, humming, and other non-verbal sounds within the audio.<br/>
+        <b>3. Speaker Identification:</b> For audio with multiple speakers, we identify and label each speaker within the segments.
 </>),
     name:"details1"
     },
@@ -17,9 +17,9 @@ export const serviceDescription=[
         description:(
         <>
             <b>• Project Initialization- Define Your Objectives:</b> We begin by understanding the specific goals of your image annotation project. This involves discussing the types of images to be annotated and the desired outcomes, ensuring that we tailor the process to meet your needs.<br/>
-            <b>• Customization- </b>We tailor our annotation process to fit the specific needs of your project, ensuring that we meet your unique requirements.<br/>
-            <b>• Image Collection -</b> Use provided dataset of images or Gather Relevant Images: Next, we compile a diverse set of images that align with your project objectives. This collection may include various categories, contexts, and scenarios to provide a comprehensive dataset for annotation.<br/>
-            <b>• Annotation Guidelines Development- </b>We develop detailed annotation guidelines that outline the specific criteria and standards for annotating the images. This ensures consistency and accuracy across all annotations, making it easier for annotators to understand their tasks.<br/>
+            <b>• Customization-</b> We tailor our annotation process to fit the specific needs of your project, ensuring that we meet your unique requirements.<br/>
+            <b>• Image Collection-</b> Use provided dataset of images or Gather Relevant Images: Next, we compile a diverse set of images that align with your project objectives. This collection may include various categories, contexts, and scenarios to provide a comprehensive dataset for annotation.<br/>
+            <b>• Annotation Guidelines Development-</b> We develop detailed annotation guidelines that outline the specific criteria and standards for annotating the images. This ensures consistency and accuracy across all annotations, making it easier for annotators to understand their tasks.<br/>
             <b>• Annotator Selection and Training-</b> We choose skilled annotators who are experienced in image annotation. Additionally, we provide training on the guidelines and tools to ensure they are well-prepared to deliver high-quality annotations.<br/>
             <b>• Annotation Process-</b> The annotators start the annotation process, labeling images according to the established guidelines. This may include identifying objects, adding descriptive tags, or verifying the images with descriptions depending on your project’s requirements.<br/>
             <b>• Quality Control and Review-</b> Throughout the annotation process, we conduct regular quality control checks to ensure accuracy and consistency. This includes reviewing a sample of annotated images and providing feedback to annotators as needed.<br/>
@@ -34,13 +34,13 @@ export const serviceDescription=[
         title:"Audio Verification Process",
         description:(
         <>
-            <b>1. Audio Segmentation:</b> We begin by dividing the provided audio file and its corresponding text file into smaller, manageable segments.<br/>
+        <b>1. Audio Segmentation:</b> We begin by dividing the provided audio file and its corresponding text file into smaller, manageable segments.<br/>
         <b>2. Guideline Creation:</b> Detailed guidelines are developed to ensure annotators clearly understand the project’s requirements.<br/>
         <b>3. Sample Set Evaluation:</b> A sample set is created and provided to native language annotators. Based on their performance on this sample set, they are selected for the project.<br/>
         <b>4. Audio and Text Verification:</b> Each audio segment is verified against the corresponding text. We check the audio for mispronounced words and incomplete speech, and the text for misspellings and incomplete content.<br/>
         <b>5. Content Analysis:</b> Each segment is meticulously analyzed for any religious, political, abusive, inappropriate, controversial, or personal content, and marked accordingly.<br/>
-        <b>6.	Native Speaker Review:</b> Each entry is then reviewed by another set of native speakers to ensure accuracy and quality.<br/>
-        <b>7.	Two-Step Review:</b> Depending on the project’s requirements, a two-step review process is conducted to ensure thoroughness and precision.
+        <b>6. Native Speaker Review:</b> Each entry is then reviewed by another set of native speakers to ensure accuracy and quality.<br/>
+        <b>7. Two-Step Review:</b> Depending on the project’s requirements, a two-step review process is conducted to ensure thoroughness and precision.
        
         </>
 
@@ -55,7 +55,7 @@ export const serviceDescription=[
             <h3><b>Types of NLP Annotations 
             </b></h3><br/>
             NLP annotations come in various forms, each tailored to specific tasks and applications. Here are some of the most common types: <br/>
-            <b>1. Entity Annotation:</b> Identifying and labeling named entities such as people, organizations, locations, and dates within the text.<br/>
+<b>1. Entity Annotation:</b> Identifying and labeling named entities such as people, organizations, locations, and dates within the text.<br/>
 <b>2. Relation Extraction:</b> Identifying and labeling the relationships between entities in the text, such as "works for" or "located in".<br/>
 <b>3. Sentiment Analysis:</b> Labeling text with sentiment tags such as positive, negative, or neutral to gauge opinions and emotions.<br/>
 <b>4. Text Classification:</b> Categorizing text into predefined classes or topics based on its content.<br/>
@@ -71,7 +71,7 @@ The NLP annotation process typically involves the following steps:<br/>
 <b>4. Annotation:</b> Labeling the text data according to the specified guidelines.<br/> 
 <b>5. Quality Assurance:</b> Reviewing and validating the annotated data to ensure accuracy and consistency, often involving multiple annotators and inter-annotator agreement measures.<br/>
 <b>6. Client Review and Feedback:</b> Once the annotations have passed our quality checks, we share them with you for review. This step ensures that the final product aligns perfectly with your expectations.<br/>
-<b>7.  Final Delivery:</b> After incorporating your feedback and making any necessary adjustments, we deliver the final annotated dataset in your preferred format. 
+<b>7. Final Delivery:</b> After incorporating your feedback and making any necessary adjustments, we deliver the final annotated dataset in your preferred format. 
 
 
         </>),
