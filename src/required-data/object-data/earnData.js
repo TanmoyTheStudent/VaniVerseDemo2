e5d202d565a0350fb8a7069f25753fddd9ab1196
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-19 08:26:36 +0530

    Update earnData.js

--- src/required-data/object-data/earnData.js
+++ src/required-data/object-data/earnData.js
@@ -2,32 +2,58 @@
 export const earnData=[
     {
         id:1,
-        title:"Audio Recording",
-        description:"Here you have to record the speech of your community speaking in your language with a valid consent so that it can be used in commercial purposes."
+        title:"Transcription",
+        description:(<>Convert audio or video content into written text.<br/>
+                        <b>Skills Required:</b> Good listening skills, attention to detail, proficiency in the relevant Indian language and typing skills in the target language.
+                    </>)
     },
     {
         id:2,
-        title:"Audio Segmentation",
-        description:"If you are good at your language, you can participate in audio segmentation activity. Audio segmentation encompasses various categories, including speaker recognition, noise segmentation, and specific sound identification."
+        title:"Translation",
+        description:(<>Translate text from one Indian language to another.<br/>
+                        <b>Skills Required:</b>  Proficiency in multiple Indian languages, cultural sensitivity, strong writing skills and focus on accuracy and cultural context.
+                    </>)
     },
     {
         id:3,
-        title:"Transcription",
-        description:"In this activity you have to transcribe speech recording of your langauge to text in your native script. Proper spelling and format would be required here. "
+        title:"Audio Recording",
+        description:(<>Record high-quality audio samples in your native Indian languages for various applications.<br/>
+                        <b>Skills Required:</b> Clear voice articulation, accurate pronunciation, proficiency in the relevant Indian language, and access to recording equipment.
+                    </>)
     },
     {
         id:4,
-        title:"Translation",
-        description:"If you are good at other language, apart from mother-tongue,you can participate in this activity. Here you have to translate text from one language to another language"
+        title:"Audio Segmentation",
+        description:(<>Segment audio recordings into distinct parts based on content or speaker changes.<br/>
+                        <b>Skills Required:</b> Good listening skills, familiarity with audio editing tools, and proficiency in the relevant Indian language.
+                    </>)
     },
-    {
+   {
         id:5,
-        title:"Studio Recording",
-        description:"Audio recording of the written text based on the requirements of language, dialect, gender, age of the speaker and the recording environment of the audio."
+        title:" Audio Verification",
+        description:(<>Verify the accuracy of transcribed audio recordings in Indian languages.<br/>
+                        <b>Skills Required:</b> Strong listening skills, attention to detail, and proficiency in the relevant Indian language.
+                    </>)
     },
     {
         id:6,
-        title:"Audio Verification",
-        description:"After the primary process, the verification may be required based on the activity.Experts of such can participate in this."
+        title:"Custom Data Curation",
+        description:(<>Curate datasets tailored to specific project requirements in Indian languages, including data cleaning and organization.<br/>
+                        <b>Skills Required:</b> Strong analytical skills, attention to detail, familiarity with Indian language data and understanding of project requirements.
+                    </>)
+    },
+    {
+        id:7,
+        title:"Image Annotations",
+        description:(<>Annotate images by labeling objects, scenes, or other relevant features.<br/>
+                        <b>Skills Required:</b> Attention to detail, Good visual recognition and understanding of Indian cultural references.
+                    </>)
+    },
+    {
+        id:8,
+        title:"NLP Annotations",
+        description:(<>Annotate text data in Indian languages for natural language processing tasks, such as sentiment analysis or named entity recognition.<br/>
+                        <b>Skills Required:</b> Understanding of linguistic concepts, attention to detail, and proficiency in the relevant Indian language.
+                    </>)
     }
 ]
