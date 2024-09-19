494b4f723d88f15e9fb736ef199d83d8b459ca8c
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-19 14:42:00 +0530

    Update STT3-new.js

--- src/components/services/sub-components/STT3-new.js
+++ src/components/services/sub-components/STT3-new.js
@@ -28,13 +28,13 @@ function STT3New() {
     }}
     >
 
-           <b>1. Project Guidelines Creation:</b> We start by crafting a comprehensive guideline to ensure our annotators fully understand the project’s objectives and requirements.<br/><br/>
-           <b>2. Audio Segmentation:</b> The provided audio file is divided into smaller, manageable segments for detailed analysis.<br/><br/>
-           <b>3. Non-Verbal Sound Identification:</b> We meticulously identify and mark pauses, noises, coughs, laughter, breathing sounds, humming, and other non-verbal sounds within the audio.<br/><br/>
-           <b>4. Speaker Identification:</b> For audio with multiple speakers, we identify and label each speaker within the segments.<br/><br/>
-           <b>5. Native Speaker Transcription:</b> Sample audio segments are given to native speakers of the language. Based on the quality of their transcriptions, they are selected for the project.<br/><br/>
-           <b>6. Content Analysis:</b> Each segment is thoroughly analyzed for any religious, political, abusive, inappropriate, controversial, or personal content, and marked accordingly.<br/><br/>
-           <b>7. Two-Step Review:</b> Depending on the project’s requirements, a two-step review process is conducted to ensure accuracy and quality.
+           <b>1. Project Guidelines Creation :</b> We start by crafting a comprehensive guideline to ensure our annotators fully understand the project’s objectives and requirements.<br/><br/>
+           <b>2. Audio Segmentation :</b> The provided audio file is divided into smaller, manageable segments for detailed analysis.<br/><br/>
+           <b>3. Non-Verbal Sound Identification :</b> We meticulously identify and mark pauses, noises, coughs, laughter, breathing sounds, humming, and other non-verbal sounds within the audio.<br/><br/>
+           <b>4. Speaker Identification :</b> For audio with multiple speakers, we identify and label each speaker within the segments.<br/><br/>
+           <b>5. Native Speaker Transcription :</b> Sample audio segments are given to native speakers of the language. Based on the quality of their transcriptions, they are selected for the project.<br/><br/>
+           <b>6. Content Analysis :</b> Each segment is thoroughly analyzed for any religious, political, abusive, inappropriate, controversial, or personal content, and marked accordingly.<br/><br/>
+           <b>7. Two-Step Review :</b> Depending on the project’s requirements, a two-step review process is conducted to ensure accuracy and quality.
 
     </Typography>
        
