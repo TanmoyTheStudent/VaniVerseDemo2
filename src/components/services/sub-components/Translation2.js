c07e4eb475f39fc7da583221f1a6a279c8bbea3b
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-19 14:41:31 +0530

    Update Translation2.js

--- src/components/services/sub-components/Translation2.js
+++ src/components/services/sub-components/Translation2.js
@@ -27,10 +27,10 @@ textAlign:"left"
 }}
 >
   
-         <b>1. Project Guidelines Creation:</b> We begin by developing detailed guidelines to ensure our annotators have a clear understanding of the project’s goals and requirements.<br/><br/>
-         <b>2. Native Speaker Translation:</b> Sample text segments are translateded by native speakers of the language. Based on the quality of their translations, they are selected for the project.<br/><br/>
-         <b>3. Content Analysis:</b> Each segment is carefully analyzed for any religious, political, abusive, inappropriate, controversial, or personal content, and marked accordingly.<br/><br/>
-         <b>4. Two-Step Review:</b> Depending on the project’s requirements, a two-step review process is conducted to ensure accuracy and quality.<br/><br/>
+         <b>1. Project Guidelines Creation :</b> We begin by developing detailed guidelines to ensure our annotators have a clear understanding of the project’s goals and requirements.<br/><br/>
+         <b>2. Native Speaker Translation :</b> Sample text segments are translateded by native speakers of the language. Based on the quality of their translations, they are selected for the project.<br/><br/>
+         <b>3. Content Analysis :</b> Each segment is carefully analyzed for any religious, political, abusive, inappropriate, controversial, or personal content, and marked accordingly.<br/><br/>
+         <b>4. Two-Step Review :</b> Depending on the project’s requirements, a two-step review process is conducted to ensure accuracy and quality.<br/><br/>
   
 </Typography>
          
