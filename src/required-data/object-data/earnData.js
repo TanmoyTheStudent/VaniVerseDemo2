cd123cc63f8eeeb690a9963f8aa5fc89fa32a075
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-09-19 15:38:15 +0530

    Update earnData.js

--- src/required-data/object-data/earnData.js
+++ src/required-data/object-data/earnData.js
@@ -3,56 +3,56 @@ export const earnData=[
     {
         id:1,
         title:"Transcription",
-        description:(<>Convert audio or video content into written text.<br/>
+        description:(<>Convert audio or video content into written text.<br/><br/>
                         <b>Skills Required:</b> Good listening skills, attention to detail, proficiency in the relevant Indian language and typing skills in the target language.
                     </>)
     },
     {
         id:2,
         title:"Translation",
-        description:(<>Translate text from one Indian language to another.<br/>
+        description:(<>Translate text from one Indian language to another.<br/><br/>
                         <b>Skills Required:</b>  Proficiency in multiple Indian languages, cultural sensitivity, strong writing skills and focus on accuracy and cultural context.
                     </>)
     },
     {
         id:3,
         title:"Audio Recording",
-        description:(<>Record high-quality audio samples in your native Indian languages for various applications.<br/>
+        description:(<>Record high-quality audio samples in your native Indian languages for various applications.<br/><br/>
                         <b>Skills Required:</b> Clear voice articulation, accurate pronunciation, proficiency in the relevant Indian language, and access to recording equipment.
                     </>)
     },
     {
         id:4,
         title:"Audio Segmentation",
-        description:(<>Segment audio recordings into distinct parts based on content or speaker changes.<br/>
+        description:(<>Segment audio recordings into distinct parts based on content or speaker changes.<br/><br/>
                         <b>Skills Required:</b> Good listening skills, familiarity with audio editing tools, and proficiency in the relevant Indian language.
                     </>)
     },
    {
         id:5,
         title:" Audio Verification",
-        description:(<>Verify the accuracy of transcribed audio recordings in Indian languages.<br/>
+        description:(<>Verify the accuracy of transcribed audio recordings in Indian languages.<br/><br/>
                         <b>Skills Required:</b> Strong listening skills, attention to detail, and proficiency in the relevant Indian language.
                     </>)
     },
     {
         id:6,
         title:"Custom Data Curation",
-        description:(<>Curate datasets tailored to specific project requirements in Indian languages, including data cleaning and organization.<br/>
+        description:(<>Curate datasets tailored to specific project requirements in Indian languages, including data cleaning and organization.<br/><br/>
                         <b>Skills Required:</b> Strong analytical skills, attention to detail, familiarity with Indian language data and understanding of project requirements.
                     </>)
     },
     {
         id:7,
         title:"Image Annotations",
-        description:(<>Annotate images by labeling objects, scenes, or other relevant features.<br/>
+        description:(<>Annotate images by labeling objects, scenes, or other relevant features.<br/><br/>
                         <b>Skills Required:</b> Attention to detail, Good visual recognition and understanding of Indian cultural references.
                     </>)
     },
     {
         id:8,
         title:"NLP Annotations",
-        description:(<>Annotate text data in Indian languages for natural language processing tasks, such as sentiment analysis or named entity recognition.<br/>
+        description:(<>Annotate text data in Indian languages for natural language processing tasks, such as sentiment analysis or named entity recognition.<br/><br/>
                         <b>Skills Required:</b> Understanding of linguistic concepts, attention to detail, and proficiency in the relevant Indian language.
                     </>)
     }
